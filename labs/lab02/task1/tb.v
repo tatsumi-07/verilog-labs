af9c1345a32e7bb2f58adbb750979f45ae8245da
Nikunj <f20240919@pilani.bits-pilani.ac.in>
2026-09-19 14:47:23 +0000

    lab02 task1

--- labs/lab02/task1/tb.v
+++ labs/lab02/task1/tb.v
@@ -8,30 +8,43 @@ module tb;
 
   // TODO: declare the three DUT inputs as the appropriate variable type.
   // Use exactly these names: t_i0, t_i1, t_s (needed by $monitor below).
-  reg   ________________________;
+  reg  t_i0, t_i1, t_s;
   // TODO: declare the DUT output as the appropriate net type.
   // Use exactly this name: t_y (needed by $monitor below).
-  wire  ________________________;
-
+  wire  t_y;
   // TODO: instantiate DUT here, connecting t_i0, t_i1, t_s, t_y to its ports
-
+DUT U1 (
+  .I0 (t_i0),
+  .I1 (t_i1),
+  .S  (t_s),
+  .Y  (t_y)
+);
 
   // Waveform dump configuration
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, tb);
     end
   end
 
   initial begin
-    // TODO: apply all 8 combinations of t_i0, t_i1, t_s, 5 time units apart,
-    // then $finish. (Same pattern you used in Lab 1's tb.v.)
-
+  // Apply all 8 combinations of t_i0, t_i1, t_s
+  {t_i0, t_i1, t_s} = 3'b000; #5;
+  {t_i0, t_i1, t_s} = 3'b001; #5;
+  {t_i0, t_i1, t_s} = 3'b010; #5;
+  {t_i0, t_i1, t_s} = 3'b011; #5;
+  {t_i0, t_i1, t_s} = 3'b100; #5;
+  {t_i0, t_i1, t_s} = 3'b101; #5;
+  {t_i0, t_i1, t_s} = 3'b110; #5;
+  {t_i0, t_i1, t_s} = 3'b111; #5;
+  $finish;
   end
+
 
   initial
     $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y);
 
 endmodule
+
